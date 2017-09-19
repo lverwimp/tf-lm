de2347acf07c34142c183aab9ef26724b8169f81
lverwimp <lyan.verwimp@telenet.be>
2017-09-19 09:34:35 +0200

    added argparse

--- aux_scripts/make_char_feat_files.py
+++ aux_scripts/make_char_feat_files.py
@@ -1,54 +1,59 @@
 #! /usr/bin/env python
 # creates character feature files
 
-import sys
-
-f = sys.argv[1] # data file containing words
-prefix = str(sys.argv[2]) # prefix for data file with characters
-it = int(sys.argv[3]) # position in the word that should be written to file, starting from 0
-invert = str(sys.argv[4]) # 'normal' or 'invert': order of the characters
-
-if invert != 'normal' and invert != 'invert':
-	raise IOError("Specify in which order the characters should be added (4th argument): \
-		starting from the beginning of the word ('normal') or from the ending of the \
-		word ('invert').")
+import sys, argparse
 
 PADDING_SYMBOL = '#' # change this if this symbol already occurs in your dataset
 
-out_f = open('{0}{1}'.format(prefix,it), 'w')
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
 
-for line in open(f, 'r'):
-	l = line.split(' ')
-	for word in l[:-1]: # for each word in the sentence except the last one
-		if word != '' and word != ' ' and word != '\n': # skip empty elements and newlines
-			word_l = list(word)
+	parser.add_argument('file', help='data file containing words')
+	parser.add_argument('prefix', help='prefix for data file with characters')
+	parser.add_argument('position', type=int, help='position in the word that should be written to file, starting from 0')
+	parser.add_argument('--invert', help='invert the order of the characters', action='store_true')
 
-			if invert == 'invert':
+	args = parser.parse_args()
+
+	if args.invert:
+		invert = True
+	else:
+		invert = False
+
+	out_f = open('{0}{1}'.format(args.prefix, args.position), 'w')
+
+	for line in open(args.file, 'r'):
+		l = line.split(' ')
+		for word in l[:-1]: # for each word in the sentence except the last one
+			if word != '' and word != ' ' and word != '\n': # skip empty elements and newlines
+				word_l = list(word)
+
+				if invert:
+					word_l = word_l[::-1]
+
+				# if e.g. length of word = 3 and it = 4 --> PADDING_SYMBOL
+				if len(word_l) < args.position+1:
+					out_f.write('{0} '.format(PADDING_SYMBOL))
+
+				# otherwise, print correct letter
+				else:
+					out_f.write(word_l[args.position] + ' ')
+
+		# last word: do the same, except add a newline instead of a space
+		word_l = list(l[-1].strip('\n'))
+		if ''.join(word_l) != '' and ''.join(word_l) != ' ':
+
+			if invert:
 				word_l = word_l[::-1]
 
-			# if e.g. length of word = 3 and it = 4 --> PADDING_SYMBOL
-			if len(word_l) < it+1:
-				out_f.write('{0} '.format(PADDING_SYMBOL))
+			# if e.g. length of word = 3 and it = 4 --> hashtag
+			if len(word_l) < args.position+1:
+				out_f.write('{0}\n'.format(PADDING_SYMBOL))
 
 			# otherwise, print correct letter
 			else:
-				out_f.write(word_l[it] + ' ')
+				out_f.write(word_l[args.position] + '\n') # word is NOT inverted
 
-	# last word: do the same, except add a newline instead of a space
-	word_l = list(l[-1].strip('\n'))
-	if ''.join(word_l) != '' and ''.join(word_l) != ' ':
-
-		if invert == 'invert':
-			word_l = word_l[::-1]
-
-		# if e.g. length of word = 3 and it = 4 --> hashtag
-		if len(word_l) < it+1:
-			out_f.write('{0}\n'.format(PADDING_SYMBOL))
-
-		# otherwise, print correct letter
 		else:
-			out_f.write(word_l[it] + '\n') # word is NOT inverted
-
-	else:
-		out_f.write('\n')
-out_f.close()
+			out_f.write('\n')
+	out_f.close()
